a04750594f3ea99400ea813c7b4e5c75c7d70369
uuymaion <129833708+uuymaion@users.noreply.github.com>
2025-11-26 16:51:59 +0800

    Create Sep17_a.java

---
+++ Sep17/Sep17_a.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Sep17_a{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int how = sc.nextInt();
+
+        for(int i=0;i<how;i++){
+            int a = sc.nextInt();
+            System.out.println(a + " is the input number");
+        }
+        sc.close();
+    }
+}
